a71de245c801aa136f6f639a60a2463d7c75dd06
CodingC1402 <CornyCodingCorn@gmail.com>
2023-07-10 18:20:21 +0700

    Finally deployed rabbitmq with consul

--- src/K8S/deploy.sh
+++ src/K8S/deploy.sh
@@ -6,5 +6,8 @@ workDir=$(dirname -- "$workDir");
 chmod +x "$workDir"/create-cluster.sh;
 "$workDir"/create-cluster.sh "bazaar" "y";
 
+chmod +x "$workDir"/deploy-consul.sh;
+"$workDir"/deploy-consul.sh
+
 chmod +x "$workDir"/helm-install-all.sh;
 "$workDir"/helm-install-all.sh;
