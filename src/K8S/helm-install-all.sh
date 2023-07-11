0890a4009cc6cffc89786837df18379b6d34b145
CodingC1402 <CornyCodingCorn@gmail.com>
2023-07-11 21:58:07 +0700

    wtf

--- src/K8S/helm-install-all.sh
+++ src/K8S/helm-install-all.sh
@@ -15,13 +15,13 @@ install rabbitmq eventbus rabbitmq y y
 install dashboard dashboard dashboard y y
 install webapigw proxy webapigw y y
 install internallb proxy internallb y y
-install webbff webbff webbff y y
 install ingressgw consulgw ingressgw y y
 install configs configs configs y y
+install webbff webbff webbff y y
 install data-volume volume data y n
 
 echo "> Waiting for rabbitmq pod"
-kubectl wait --namespace default --for=condition=ready pod/rabbitmq-0 --timeout=30s
+kubectl wait --namespace default --for=condition=ready pod/rabbitmq-0 --timeout=240s
 
 
 "$workDir"/helm-install-services.sh
